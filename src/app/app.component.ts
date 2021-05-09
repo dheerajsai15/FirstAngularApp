e34b2686417b1ba0450deb224bee188e7edfb990
Dheeraj sai <dheerajsai15@gmail.com>
2021-05-09 21:10:04 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MyAngularApp1';
+  Users:object[]=[];
+  bool:boolean=false;
+  addUserData(ref:NgForm)
+  {
+       let UserObj=ref.value;
+       this.bool=true;
+       this.Users.push(UserObj);
+       ref.reset();
+  }
+  Del(ind:number)
+  {
+      this.Users.splice(ind,1);
+  }  
+}
