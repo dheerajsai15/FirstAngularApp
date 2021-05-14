b63ae640003aed9079c72cd302287bcee3c0c5e5
Dheeraj sai <dheerajsai15@gmail.com>
2021-05-14 20:50:07 +0530

    routing & navigation is implemented

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../Models/user.model';
+
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProductDetailsComponent{
+  @Input() productObject:Product;
+ 
 }
